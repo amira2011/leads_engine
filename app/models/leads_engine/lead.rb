c686f2814899c890c808c7a72c4fc9f702258c16
amira2011 <shaikh.abidali@gmail.com>
2024-06-20 20:45:57 +0530

    Updated Model

--- app/models/leads_engine/lead.rb
+++ app/models/leads_engine/lead.rb
@@ -1,4 +1,3 @@
-Resolving dependencies...
 module LeadsEngine
   class Lead < ApplicationRecord
     self.table_name = "leads"
--- lib/leads_engine/version.rb
+++ lib/leads_engine/version.rb
@@ -1,3 +1,3 @@
 module LeadsEngine
-  VERSION = "0.1.2"
+  VERSION = "0.1.3"
 end
