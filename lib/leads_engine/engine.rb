2034bfc8e154b291bd5949e045f92c5378ef6b8b
amira2011 <shaikh.abidali@gmail.com>
2024-07-03 22:59:43 +0530

    database Config update10

--- lib/leads_engine/engine.rb
+++ lib/leads_engine/engine.rb
@@ -1,11 +1,13 @@
+require 'rails/engine'
 require_relative 'application_configuration'
+require_relative 'database_configuration'
 
 module LeadsEngine
   
 
   class Engine < ::Rails::Engine
     isolate_namespace LeadsEngine
-    config.application = ApplicationConfiguration.new
+    config.application = LeadsEngine::ApplicationConfiguration.new
 
     def self.table_name_prefix
       config.application.database.table_name_prefix
--- lib/leads_engine/version.rb
+++ lib/leads_engine/version.rb
@@ -1,3 +1,3 @@
 module LeadsEngine
-  VERSION = "0.1.9"
+  VERSION = "0.1.10"
 end
